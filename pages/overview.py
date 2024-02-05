4a2ac86f22b8b1035a54d1a558aa098d02471643
Wan Asyraf <107595740+wanasyraf4@users.noreply.github.com>
2024-02-06 04:43:07 +0800

    Add files via upload

--- pages/overview.py
+++ pages/overview.py
@@ -16,7 +16,7 @@ from datetime import datetime, timedelta
 
 # Function to update the graph
 def update_graph(n_intervals):
-    now = datetime.now()
+    now = datetime.now() + timedelta(hours=16)
 
     # Append new time and value
     x_values.append(now)
